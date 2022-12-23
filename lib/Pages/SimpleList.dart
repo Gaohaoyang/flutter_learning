7dc04896249da3959f70b697ad3929bd25122ab1
Haoyang <gaohaoyang126@126.com>
2022-12-23 12:20:05 +0800

    feat: 🎸 update

--- lib/Pages/SimpleList.dart
+++ lib/Pages/SimpleList.dart
@@ -8,7 +8,7 @@ class SimpleList extends StatefulWidget {
 }
 
 class _SimpleListState extends State<SimpleList> {
-  List<int> list = [1, 2, 3, 4, 5, 6, 7];
+  List<int> list = [1, 2, 3, 4, 5, 6, 7, 8];
 
   @override
   Widget build(BuildContext context) {
@@ -17,10 +17,16 @@ class _SimpleListState extends State<SimpleList> {
         title: const Text('Simple List'),
         centerTitle: true,
       ),
-      body: Column(
-        children: list.map((item) => Text(
-          '$item'
-        )).toList(),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: list.map((item) => SizedBox(
+            height: 30,
+            child: Text(
+              '$item',
+            ),
+          )).toList(),
+        ),
       ),
     );
   }
